abbb86c8b2e85564417d242e2420f070d33692f1
Mariusz <smajdormariusz98@gmail.com>
2022-05-04 13:57:32 +0200

    Connected themes for all files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App';
+import { theme } from './theme';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  color: {
+    primary: '#064f32',
+    primaryLighter: '#06603c',
+    text: '#fcfcfc',
+  },
+  breakpoint: {
+    small: 575,
+    medium: 767,
+    big: 991,
+    large: 1199,
+  },
+  layout: {
+    navSize: 75,
+  },
+  time: {
+    transition: 0.5,
+  },
+};
