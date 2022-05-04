19aa75ab0320ba502388dafa16e6be4eaf79eec1
Mariusz <smajdormariusz98@gmail.com>
2022-05-04 13:59:26 +0200

    Applied global styles

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
 import App from './App';
+import { GlobalStyle } from './globalStyle';
 import { theme } from './theme';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </StrictMode>
