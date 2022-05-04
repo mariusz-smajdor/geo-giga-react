228a61b5789744c366445015e79558505a077606
Mariusz <smajdormariusz98@gmail.com>
2022-05-04 17:30:52 +0200

    Added padding into Container component

--- src/components/layout/Container/styled.js
+++ src/components/layout/Container/styled.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.main`
-  margin-left: ${({ theme }) => theme.layout.navbar};
+  margin-left: ${({ theme }) => theme.layout.navbar}px;
+  padding: 25px;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100vh;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
     margin-left: unset;
-    margin-bottom: ${({ theme }) => theme.layout.smallNavbar};
+    margin-bottom: ${({ theme }) => theme.layout.smallNavbar}px;
   }
 `;
