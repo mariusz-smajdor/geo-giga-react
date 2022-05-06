a912786afe5edd1585a7251589298ac18093af40
Mariusz <smajdormariusz98@gmail.com>
2022-05-06 15:56:41 +0200

    Removed url prefix

--- src/getCountries.js
+++ src/getCountries.js
@@ -1,7 +1,5 @@
-import { url_prefix } from '../src/url-config';
-
 export async function getCountries() {
-  const response = await fetch(`${url_prefix}/countries.json`);
+  const response = await fetch(`/geo-giga-react/countries.json`);
 
   if (!response.ok) {
     new Error(response.statusText);
--- src/url-config.js
+++
@@ -1,4 +0,0 @@
-export const url_prefix =
-  !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
-    ? '/geo-giga-react'
-    : '';
