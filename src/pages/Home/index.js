b0217ad9d15ce52822f004d516ff36b88c4cda24
Mariusz <smajdormariusz98@gmail.com>
2022-05-25 12:30:04 +0200

    Refactored homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,36 @@
 import Container from '../../components/layout/Container';
-import { Wrapper, Title, Text, StyledLink } from './styled';
+import { IconContext } from 'react-icons';
+import { RiFlagLine, RiRoadMapLine, RiEarthLine } from 'react-icons/ri';
+import { Wrapper, Title, Text, Games, Game, StyledLink } from './styled';
 
 function Home() {
   return (
     <Container>
       <Wrapper>
         <Title>Welcome to the Geo Giga!</Title>
-        <Text>
-          You can choose between our various games such as{' '}
-          <StyledLink to='flags'>Flags Game</StyledLink>,{' '}
-          <StyledLink to='street-view'>Street View Game</StyledLink> and{' '}
-          <StyledLink to='/'>Lands Game</StyledLink>
-        </Text>
+        <Text>Choose the game you want to play</Text>
+        <Games>
+          <IconContext.Provider value={{ size: '25px' }}>
+            <StyledLink to='flags'>
+              <Game>
+                <RiEarthLine />
+                Flags Game
+              </Game>
+            </StyledLink>
+            <StyledLink to='street-view'>
+              <Game>
+                <RiRoadMapLine />
+                Street View Game
+              </Game>
+            </StyledLink>
+            <StyledLink to='/'>
+              <Game>
+                <RiFlagLine />
+                Lands Game
+              </Game>
+            </StyledLink>
+          </IconContext.Provider>
+        </Games>
       </Wrapper>
     </Container>
   );
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
-  max-width: ${({ theme }) => theme.breakpoint.small}px;
+  max-width: ${({ theme }) => theme.breakpoint.medium}px;
   border-radius: 20px;
   box-shadow: ${({ theme }) => theme.color.secondary} 0px 2px 8px 0px;
   background: ${({ theme }) => theme.color.primary};
@@ -17,6 +17,25 @@ export const Title = styled.h2`
 
 export const Text = styled.p`
   line-height: 1.5;
+`;
+
+export const Games = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: 1fr;
+  text-align: center;
+  gap: 20px;
+`;
+
+export const Game = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+  border: 2px solid;
+  border-radius: 20px;
 `;
 
 export const StyledLink = styled(Link)`
