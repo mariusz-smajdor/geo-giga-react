18de7a1bdc293398a5ddab3d61eb6d1a7cba5940
Mariusz <smajdormariusz98@gmail.com>
2022-05-25 13:27:13 +0200

    Lands component

---
+++ src/pages/Lands/index.js
@@ -0,0 +1,11 @@
+import Container from '../../components/layout/Container';
+
+function Lands() {
+  return (
+    <Container>
+      <h1>hello</h1>
+    </Container>
+  );
+}
+
+export default Lands;
