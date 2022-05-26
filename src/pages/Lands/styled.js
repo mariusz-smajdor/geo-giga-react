f1a11e162eaa7854d1e91533a3af1689f90d49b0
Mariusz <smajdormariusz98@gmail.com>
2022-05-26 13:12:33 +0200

    Positioned the map

--- src/pages/Lands/styled.js
+++ src/pages/Lands/styled.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 export const Map = styled.section`
+  display: flex;
+  justify-content: center;
   height: 100vh;
   width: calc(100% - ${({ theme }) => theme.layout.navbar}px);
   margin-left: ${({ theme }) => theme.layout.navbar}px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    width: 100%;
+    margin-left: 0;
+    height: calc(100vh - ${({ theme }) => theme.layout.smallNavbar}px);
+  } ;
 `;
