4afcc7c486e3fce81d552a7472bf2c7a85787e09
Mariusz <smajdormariusz98@gmail.com>
2022-06-06 13:05:50 +0200

    Removed map from react simple maps

--- src/pages/Lands/index.js
+++ src/pages/Lands/index.js
@@ -1,31 +1,5 @@
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  ZoomableGroup,
-} from 'react-simple-maps';
-
-import { Map } from './styled';
-
-const geoUrl =
-  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
-
 function Lands() {
-  return (
-    <Map>
-      <ComposableMap>
-        <ZoomableGroup>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map(geo => {
-                return <Geography key={geo.rsmKey} geography={geo} />;
-              })
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-    </Map>
-  );
+  return <span>lands game</span>;
 }
 
 export default Lands;
